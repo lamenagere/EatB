59a1d6a7f2a5e22819cdd4f9b4630bd1ae51d280
Math <mdemaleingreau@gmail.com>
2019-02-18 15:46:21 +0100

    ajout des modeles kitchen et kitchentype

---
+++ EatBrusselsAPI/EatBrussels/Models/Kitchen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EatBrussels.Models
+{
+    public class Kitchen
+    {
+        [Key]
+        public int KitchenID { get; set; }
+        public int RestaurantID { get; set; }
+        
+
+    }
+}
---
+++ EatBrusselsAPI/EatBrussels/Models/KitchenType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EatBrussels.Models
+{
+    public class KitchenType
+    {
+        [Key]
+        public int KitchenTypeID { get; set; }
+        public string KitchenLabel { get; set; }
+    }
+}
