fde31a29088675dde68c3692d2cca653c7d02ea6
Math <mdemaleingreau@gmail.com>
2019-02-19 15:43:05 +0100

    création du commentController, test de post de coms ok, get des commentaires par id de resto ok

---
+++ EatBrusselsAPI/EatBrussels/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatBrussels.Models
+{
+    public class CommentModel
+    {
+        public int commentID { get; set; }
+        public string userComment { get; set; }
+        public int userID { get; set; }
+        public int restaurantID { get; set; }
+    }
+}
