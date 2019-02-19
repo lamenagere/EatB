957cc1698886b870fd9df05dfc4d1c10eb98eecd
Math <mdemaleingreau@gmail.com>
2019-02-19 10:58:56 +0100

    création du restaurantModel

---
+++ EatBrusselsAPI/EatBrussels/Models/RestaurantModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatBrussels.Models
+{
+    public class RestaurantModel
+    {
+        public int restaurantID { get; set; }
+        public string name { get; set; }
+        public string kitchenType { get; set; }
+        public string address { get; set; }
+        public string openingHours { get; set; }
+        public string closingHours { get; set; }
+        public string description { get; set; }
+    }
+}
