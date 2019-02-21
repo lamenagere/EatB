e8fcd9bef290d143f230d08d6c5135eb3bdcbd80
Martin Adriaensen <martinadriaensen@gmail.com>
2019-02-21 14:43:04 +0100

    SUSU MA MAN ON PEUT POSTER DES RESTOS

--- EatBrusselsClient/src/views/AddResto.vue
+++ EatBrusselsClient/src/views/AddResto.vue
@@ -4,14 +4,14 @@
         <form class="form">
             <div>
             <label for="kitchenType">Type de cuisine </label>
-            <select id="kitchenType" v-model="restaurant.kitchenType">
-                <option value="asiatique">Asiatique</option>
-                <option value="américaine">Américaine</option>
-                <option value="européenne">Européenne</option>
-                <option value="belge">Belge</option>
-                <option value="italienne">Italienne</option>
-                <option value="healthy">Healthy</option>
-            </select>
+            <!-- <select id="kitchenType" v-model="restaurant.kitchenTypes">
+                <option :value="kt.kitchenTypeID" v-for="kt in allKitchenTypes" :key="kt.kitchenTypeID">{{kt.kitchenTypeLabel}}</option>
+            </select> -->
+            <div  v-for="kt in allKitchenTypes" :key="kt.kitchenTypeID">
+                <input type="checkbox" :id="kt.kitchenTypeLabel" :value="kt.kitchenTypeID" v-model="restaurant.kitchenTypes">
+                <label :for="kt.kitchenTypeLabel">{{kt.kitchenTypeLabel}}</label>
+            </div>
+            {{restaurant.kitchenTypes}}
             </div>
             <div>
                 <label for="name">Nom du restaurant </label>
@@ -23,9 +23,9 @@
             </div>
             <div>
                 <label for="name">Code postal communal </label>
-                <input id="name" type="number" v-model="restaurant.zipCode">
+                <input id="name" type="text" v-model="restaurant.zipCode">
             </div>
-            <button @click="post()">Ajouter ce restaurant à la liste</button>
+            <button @click.prevent="post()">Ajouter ce restaurant à la liste</button>
         </form>
     </div>
 </template>
@@ -36,18 +36,36 @@ export default {
     data: function() {
         return {
         restaurant: {
-            kitchenType: '',
+            kitchenTypes: [],
             name: '',
             address:'',
             zipCode:'',
-            }
+            },
+        allKitchenTypes: [{
+            kitchenTypeID: 1,
+            kitchenTypeLabel: "Belge"
+        },
+        {
+            kitchenTypeID: 2,
+            kitchenTypeLabel: "Asiatique"
+        }],
         }
+    },
+    created(){
+        //appeler URL qui renvoie un JSON de tous les kitchentypes
+        //et les mettre dans allKitchenTypes
+
+        // axios
+        // .get("http://localhost:63980/api/restaurants?kitchenType=" + this.$route.params.kitchenType)
+        //     .then(response => {
+        //         this.restoPerKitchenType = response.data  
+        //     })
     },
     methods: {
         post(){
             axios.post("http://localhost:63980/api/Restaurants", this.restaurant)
                 .then(response => {
-                    this.restaurant.kitchenType = '';
+                    this.restaurant.kitchenTypes = [''];
                     this.restaurant.name = '';
                     this.restaurant.address = '';
                     this.restaurant.zipCode = '';
